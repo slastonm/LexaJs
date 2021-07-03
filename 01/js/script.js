b4d513b7021d014c6229e94b34083a03650a614d
maxSlast <slastonm@gmail.com>
2021-07-03 08:50:45 +0300

    first exercise

---
+++ 01/js/script.js
@@ -0,0 +1,16 @@
+alert("Hello Lexa");
+
+// 1 Исправь ошибки в файле чтоб у тебя была чистая консоль
+let city = "Che",
+    bike = "Pride",
+    longDistanse = 130,
+    sport = ['bike', 'run', 'rekon'];
+let const = "Hello";
+let name = "Lexa";
+let age = 29;
+const date = '03.07.2021';
+let name = "Max";
+date = prompt('Укажыте датту');
+
+//2 Выведи с помощью метода console.log значения полученый с помощью команд confirm и prompt. Изучи вот ету тему еще
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
