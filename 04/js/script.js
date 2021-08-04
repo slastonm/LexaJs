498fce8947614f1729016dfbd15aad84907faacd
maxSlast <slastonm@gmail.com>
2021-08-04 15:45:37 +0300

    simple mathematical operations

---
+++ 04/js/script.js
@@ -0,0 +1,21 @@
+// Data type js
+
+window.addEventListener('DOMContentLoaded', (event) => {
+   let areaText = document.querySelector('.area span');
+   let squareAreaText = document.querySelector('.square-area span');
+   let roundAreaText = document.querySelector('.round-area span');
+
+   let sideA = 10;
+   let sideB = 20;
+   let radius = 15;
+
+    let area = "";
+    let squareArea = "";
+    let roundArea = "";
+
+    areaText.innerHTML=area;
+    squareAreaText.innerHTML=squareArea;
+    roundAreaText.innerHTML=roundArea;
+
+
+});
